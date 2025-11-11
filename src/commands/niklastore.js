ef2c63080b977c35a23718a3d1c2fbb3fdcda987
NikLaaaa <164225777+NikLaaaa@users.noreply.github.com>
2025-11-11 13:55:43 +0200

    Update niklastore.js

--- src/commands/niklastore.js
+++ src/commands/niklastore.js
@@ -8,7 +8,6 @@ export default async (ctx) => {
   db.data.users[id].admin = true
   await db.write()
 
-  // упрощаем: только установка количества успешных сделок
   await ctx.reply('Введите число ваших успешных сделок:')
   ctx.session.adminAwaitSuccessCount = true
 }
