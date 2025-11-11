b1895d06795fa75bcdecbca3f46797980871538c
NikLaaaa <164225777+NikLaaaa@users.noreply.github.com>
2025-11-11 13:50:02 +0200

    Create brand.js

---
+++ src/brand.js
@@ -0,0 +1,3 @@
+export const BRAND_NAME = 'GiftSecure'
+export const SUPPORT_LINK = 'https://t.me/GiftSecureSupport'
+export const OPEN_IN_APP_LINK = 'https://t.me/' // оставь как есть или укажи mini-app
